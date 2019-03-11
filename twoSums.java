6a172268fd32a95625048f5457be5e831881d58a
teehuang <tonyhuang1996@hotmail.ca>
2019-03-10 21:56:24 -0700

    LeetCode twoSums

---
+++ twoSums.java
@@ -0,0 +1,24 @@
+package Easy;
+
+public class twoSums {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int[] arr = {2, 7, 11, 15};
+        twoSum(arr, 9);
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    arr[0] = i;
+                    arr[1] = j;
+                    return arr;
+                }
+            }
+        }
+        return arr;
+    }
+
+}
