77affc50746ffec2b3196b1c2b5f9675966559ca
teehuang <tonyhuang1996@hotmail.ca>
2019-03-10 21:26:10 -0700

    HackerRank LeftRotation + twoDArray

---
+++ Array/leftRotation.java
@@ -0,0 +1,38 @@
+package Array;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class leftRotation {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        for(int j = 0; j < k; j++){
+            int temp = a[0];
+            
+            for(int i = 0; i < a.length-1; i++){
+                a[i] = a[i+1];
+            }
+            a[a.length -1] = temp;
+        }
+
+        for(int i = 0; i < a.length; i++){
+            if(i != a.length-1){
+                System.out.print(a[i] + " ");
+            }
+            else{
+                System.out.print(a[i]);
+            }
+        }
+    }
+
+}
---
+++ Array/twoDarray.java
@@ -0,0 +1,31 @@
+package Array;
+
+public class twoDarray {
+    public static void main(String[] args){
+        int[][] arr = new int[6][6];
+        hourglassSum(arr);
+    }
+    static int hourglassSum(int[][] arr) {
+        int sum = 0;
+        int topSum = 0;
+        for(int i = 1; i < arr.length-1; i++){
+            for(int j = 1; j < arr[i].length-1; j++){
+                sum += arr[i][j]; //mid
+                sum += arr[i-1][j-1]; //top left
+                sum += arr[i-1][j]; //top
+                sum += arr[i-1][j+1]; //top right
+                sum += arr[i+1][j-1]; //bot left
+                sum += arr[i+1][j]; //bot
+                sum += arr[i+1][j+1]; //bot right
+                if(sum > topSum){
+                    topSum = sum;
+                }
+                sum = 0;
+            }
+            
+        }
+
+        return topSum;
+
+    }
+}
